1df6c79ba1cb1cd2f6db3811f205c618aaa6844e
dinesh <dineshkumarp1206@gmail.com>
2026-09-05 13:12:00 +0530

    updated

--- backend/routes/contentRoutes.js
+++ backend/routes/contentRoutes.js
@@ -7,7 +7,7 @@ const path = require('path');
 // Multer Config for Video Uploads
 const videoStorage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/videos/');
+    cb(null, path.join(__dirname, '../uploads/videos/'));
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
@@ -22,7 +22,7 @@ const uploadVideo = multer({
 // Multer Config for Material Uploads
 const materialStorage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/materials/');
+    cb(null, path.join(__dirname, '../uploads/materials/'));
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
